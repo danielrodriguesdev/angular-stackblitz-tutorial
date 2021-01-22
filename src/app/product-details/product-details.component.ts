188311508d5eef95b320855e0f36dd9daeb8faf4
danielrodriguesdev <daniel_r_santos@outlook.com>
2021-01-22 19:26:13 -0300

    Criado serviço de Cart e Button Buy

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { products } from "../products";
+import { CartService } from "../cart.service";
 
 @Component({
   selector: "app-product-details",
@@ -9,12 +10,20 @@ import { products } from "../products";
 })
 export class ProductDetailsComponent implements OnInit {
   product;
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
   ngOnInit() {
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get("productId"));
 
     this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert("Your Product has been added to the cart!");
   }
 }
